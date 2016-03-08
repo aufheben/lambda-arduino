f7ba5c477365bb0198a31c40d3d191848b23aa1b
Yifan Yu <yifany@metropolia.fi>
2016-03-08 13:15:35 +0200

    Refactor homepage

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,6 +2,7 @@ module Handler.Home where
 
 import Global
 import Import
+import Text.Blaze
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
@@ -13,6 +14,8 @@ import Import
 getHomeR :: Handler Html
 getHomeR = do
   usb_port <- readIORef gUsbPort
+  let cols = [15, 14..0] :: [Int]
+      [col_3, col_2, col_1, col_0] = take4 cols
   defaultLayout $ do
     addStylesheet $ StaticR css_nv_d3_css
     addStylesheet $ StaticR css_bootstrap_editable_css
@@ -20,6 +23,37 @@ getHomeR = do
     addStylesheet $ StaticR css_bootstrap_slider_min_css
     setTitle "λ-arduino"
     $(widgetFile "homepage")
+  where
+  take4 [] = []
+  take4 xs = take 4 xs : take4 (drop 4 xs)
+
+dropdown_button :: Int -> Bool -> t -> Markup
+dropdown_button i pwm = [hamlet|
+    <div .col-md-3 .btn-cell>
+      <button #btn-d#{i} type="button" .btn .btn-primary .btn-circle .btn-lg .dropdown-toggle data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        D#{i}
+      <ul .dropdown-menu>
+        <li>
+          <a #btn-d#{i}-i .dropdown-item href="#">
+            Input
+        <li>
+          <a #btn-d#{i}-o .dropdown-item href="#">
+            Output
+        $if pwm
+          <li>
+            <a #btn-d#{i}-p .dropdown-item href="#">
+              PWM
+        <li>
+          <a #btn-d#{i}-u .dropdown-item href="#">
+            Unused
+  |]
+
+analog_button :: Int -> t -> Markup
+analog_button i = [hamlet|
+    <div .col-md-3 .btn-cell>
+      <button #btn-a#{i} type="button" .btn .btn-info    .btn-circle .btn-lg>
+        A#{i}
+  |]
 
 postHomeR :: Handler Html
 postHomeR = return $ toHtml ("POST not implemented" :: Text)
